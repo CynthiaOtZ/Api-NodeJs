d174cfe1b0dc5b4b9c61819dbd6ba5eb661d377d
CynthiaOtZ <cynthia@adhok.mx>
2023-01-31 14:15:56 -0600

    Eliminar tarea

--- routes/eliminar.js
+++ routes/eliminar.js
@@ -1,13 +1,17 @@
+const { LOADIPHLPAPI } = require('dns');
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+let TAREAS = [];
+let planner = [];
+let USER;
 
 
 /* GET home page. */
 router.get('/', function(peticion, respuesta) {
     //respuesta.render('eliminar');
     
-    let planner = [];
+    planner = [];
     var idTarea = 0;
 
     //Obtener datos del archivo JSON
@@ -19,42 +23,51 @@ router.get('/', function(peticion, respuesta) {
             planner.push(JSON.parse(datos));
         
             for (let i=0; i<planner.length;i++)
-            {                
-                if(planner[i].TASK.length >= 0)
-                {
-                    var limite = planner[i].TASK.length - 1;
-                    idTarea = parseInt(planner[i].TASK[limite].id) + 1;
-                                       
-                }
+            {
+                TAREAS.push(planner[i].TASK)
+                //console.log(TAREAS[0]);
+                USER = planner[i].USER.id;
             } 
-            //respuesta.render('tareas', {user: USER, task: TASK})
-            plannerString = JSON.stringify(planner[0]);
-
-            //Sobre-Escribir archivos con modulo FS (si no encuentra el archivo, lo va a crear)
-            fs.writeFile('./public/archivos/planner2023.json', plannerString, (error)=>{
-                if(error)
-                {
-                console.log("ERROR");
-                throw error;
-                }
-                else 
-                {
-                console.log("Archivo modificado exitosamente");
-                //Debemos llamar la ruta de /tareas
-                respuesta.redirect('/tareas/'+ USER);
-                }
-            });
+            respuesta.render('eliminar', {task: TAREAS[0]})
+            //plannerString = JSON.stringify(planner[0]);
         }
   })
 
 
-
-
-
-
-
-
-
-
   });
+
+
+  router.post('/', function(peticion, respuesta) {
+
+    let idTarea = peticion.body.desc;
+    let TASK = [];
+    let plannerString;
+
+    let index = TAREAS[0].findIndex(obj => obj.id==idTarea.toString());
+    TAREAS[0].splice(index,1);
+    
+    plannerString = JSON.stringify(planner[0]);
+
+    console.log("Aqui");
+    console.log(plannerString);
+
+    //Sobre-Escribir archivos con modulo FS (si no encuentra el archivo, lo va a crear)
+    fs.writeFile('./public/archivos/planner2023.json', plannerString, (error)=>{
+      if(error)
+      {
+        console.log("ERROR");
+        throw error;
+      }
+      else 
+      {
+        console.log("Archivo modificado exitosamente");
+        //Debemos llamar la ruta de /tareas
+        respuesta.redirect('/tareas/'+ USER);
+      }
+    });
+
+
+    
+  });
+
 module.exports = router;
